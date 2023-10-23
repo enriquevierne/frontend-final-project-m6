f14692611d16aa96f9b553f3a5a807c4236657f6
Enrique Vierne <110237705+enriquevierne@users.noreply.github.com>
2023-10-23 10:06:10 -0300

    feat: add cardComment component

--- src/Components/Comments/CardComment/index.tsx
+++ src/Components/Comments/CardComment/index.tsx
@@ -1,3 +1,20 @@
 export const CardComment = () => {
-    return <h2>CardComment</h2>
-}
+  return (
+    <div className="w-11/12 max-w-[663px] flex flex-col gap-2">
+      <div className="flex items-center gap-2">
+        <span className="p-1 rounded-full bg-green-800">CS</span>
+        <span className="text-gray-900 font-semibold">Camila Silva</span>
+        <span className="text-gray-500">•</span>
+        <span className="text-gray-500">há 1 mês</span>
+      </div>
+      <div className="w-full">
+        <p className="text-gray-800">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci
+          sequi culpa quaerat excepturi magnam consectetur esse facere eaque cum
+          deleniti, impedit dignissimos aperiam tenetur illum inventore itaque
+          in, repudiandae neque.
+        </p>
+      </div>
+    </div>
+  );
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,14 +9,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
-    colors: {
-      brand1: "#4529E6",
-      brand2: "#5126ea",
-      brand3: "#b0a6f0",
-      brand4: "#edeafd"
-    }
+    extend: {
+      colors: {
+        brand1: "#4529E6",
+        brand2: "#5126ea",
+        brand3: "#b0a6f0",
+        brand4: "#edeafd",
+      },
+    },
   },
   plugins: [],
-}
-
+};
