2d9460837cd4a807b6764512930d0da6dc5cbdf8
Enrique Vierne <110237705+enriquevierne@users.noreply.github.com>
2023-10-23 08:36:23 -0300

    feat: add modal component

--- src/Components/Modal/index.tsx
+++ src/Components/Modal/index.tsx
@@ -1,3 +1,17 @@
+import { ButtonDefault } from "../Form/Buttons";
+
 export const Modal = () => {
-    return <h2>Modal</h2>
-}
+  return (
+    <div className="w-11/12 m-auto rounded-md">
+      <div className="w-full p-4 flex justify-between">
+        <span className="font-bold">Sucesso!</span>
+        <span className="text-lg text-gray-400">X</span>
+      </div>
+      <div className="w-full p-4 flex flex-col gap-4">
+        <p className="font-bold">Sua conta foi criada com sucesso!</p>
+        <p className="text-gray-700">Agora você poderá ver seus negócios crescendo em grande escala</p>
+        <ButtonDefault />
+      </div>
+    </div>
+  );
+};
