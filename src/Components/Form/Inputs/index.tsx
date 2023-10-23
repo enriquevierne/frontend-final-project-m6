d2ae46544ef3d53032935e673d5f05eeceee4075
Enrique Vierne <110237705+enriquevierne@users.noreply.github.com>
2023-10-23 08:24:15 -0300

    feat: add input component

--- src/Components/Form/Inputs/index.tsx
+++ src/Components/Form/Inputs/index.tsx
@@ -1,3 +1,6 @@
 export const Input = () => {
-    return <h2>Input</h2>
+    return <input
+    className="p-2 h-8 rounded-md bg-transparent outline-none border-none text-gray-800"
+    type="text"
+  />
 }
