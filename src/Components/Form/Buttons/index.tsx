e0228ce70ce68e2177990bff3352e0e5b3583076
Enrique Vierne <110237705+enriquevierne@users.noreply.github.com>
2023-10-23 08:23:11 -0300

    feat: add header component

--- src/Components/Form/Buttons/index.tsx
+++ src/Components/Form/Buttons/index.tsx
@@ -1,4 +1,4 @@
-export const ButtonDefault = ({content}) => {
+export const ButtonDefault = () => {
   return (
     <button type="submit" className="flex items-center justify-center text-sm p-2 h-10 bg-purple-800 rounded hover:bg-gray-800 text-gray-100 font-semibold ease-int-out duration-200 uppercase">
       botão
--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,3 +1,7 @@
 export const Header = () => {
-    return <h2>Header</h2>
-}
+  return (
+    <header className="w-full bg-gray-50 m-auto flex justify-center items-center border-b shadow-md p-4">
+      <div className="w-full max-w-7xl flex justify-between items-center"></div>
+    </header>
+  );
+};
