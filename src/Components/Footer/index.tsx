5f814f992d8dedda3e6249f448fc78cb10c3b956
Enrique Vierne <110237705+enriquevierne@users.noreply.github.com>
2023-10-23 08:26:04 -0300

    feat: add footer component

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -1,3 +1,9 @@
 export const Footer = () => {
-  return <h2>Footer</h2>;
+  return (
+    <footer>
+      <div>
+        <div className="footer-copyright text-center py-3 text-xs border-t text-gray-800 font-bold"></div>
+      </div>
+    </footer>
+  );
 };
